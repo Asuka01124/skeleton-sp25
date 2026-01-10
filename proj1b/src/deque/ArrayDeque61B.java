15364b90b0e984f8fa3e66c67a538ca2ad2e186f
Asuka01124 <ziliny175@gmail.com>
2026-01-10 23:34:04 +0800

    proj1b no test save

---
+++ proj1b/src/deque/ArrayDeque61B.java
@@ -0,0 +1,104 @@
+package deque;
+import java.lang.Math;
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.ArrayList;
+
+public class ArrayDeque61B<T> implements Deque61B<T>{
+    private T[] items;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    public ArrayDeque61B() {
+        items = (T[]) new Object[8];
+        size = 0;
+        nextFirst = 0;
+        nextLast = 1;
+    }
+
+    @Override
+    public void addFirst(T x) {
+        if(size == items.length) {
+            resize(size * 2);
+        }
+        items[nextFirst] = x;
+        nextFirst = Math.floorMod(nextFirst - 1, items.length);
+        size++;
+    }
+
+    @Override
+    public void addLast(T x) {
+        if(size == items.length) {
+            resize(size * 2);
+        }
+        items[nextLast] = x;
+        nextLast = Math.floorMod(nextLast + 1, items.length);
+        size++;
+    }
+
+    @Override
+    public T get(int index) {
+        if(index < 0 || index >= size) {
+            return null;
+        }
+        return items[Math.floorMod(nextFirst + 1 + index, items.length)];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> list = new ArrayList<>();
+        for(int i = 0; i < size; i++) {
+            list.add(get(i));
+        }
+        return list;
+    }
+
+    @Override
+    public T removeFirst() {
+        if(size == 0) {
+            return null;
+        }
+        nextFirst = Math.floorMod(nextFirst + 1, items.length);
+        T item = items[nextFirst];
+        items[nextFirst] = null;
+        size--;
+        return item;
+    }
+
+    @Override
+    public T removeLast() {
+        if(size == 0) {
+            return null;
+        }
+        nextLast = Math.floorMod(nextLast - 1, items.length);
+        T item = items[nextLast];
+        items[nextLast] = null;
+        size--;
+        return item;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        throw new UnsupportedOperationException("No need to implement getRecursive for proj 1b");
+    }
+
+    private void resize(int capacity) {
+        T[] newItems = (T[]) new Object[capacity];
+        for(int i = 0; i < size; i++) {
+            newItems[i] = get(i);
+        }
+        items = newItems;
+        nextFirst = capacity - 1;
+        nextLast = size;
+    }
+}
