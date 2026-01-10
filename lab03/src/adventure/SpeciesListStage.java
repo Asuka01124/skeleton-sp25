ebdfab8830b3f8a11922f02891a1653ff065881e
Asuka01124 <ziliny175@gmail.com>
2026-01-10 20:21:04 +0800

    lab03 save

--- lab03/src/adventure/SpeciesListStage.java
+++ lab03/src/adventure/SpeciesListStage.java
@@ -95,12 +95,15 @@ public class SpeciesListStage implements AdventureStage {
     public static int arraySimilarity(List<String> listOne, List<String> listTwo) {
         List<String> copy = new ArrayList<>(listOne);
         int similarObjects = 0;
+        if(listOne.size() == 0 && listTwo.size() == 0) {
+            return 1;
+        }
         for (String o : listTwo) {
             if (copy.contains(o)) {
                 similarObjects++;
                 copy.remove(o);
             }
         }
-        return similarObjects / listOne.size();
+        return listOne.size() == 0 ? 0 : similarObjects / listOne.size();
     }
 }
